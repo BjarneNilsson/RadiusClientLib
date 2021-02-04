d0f2879945fc39cb55fe8737eb0068c1ad269c7b
bn-1980 bn-1980 <bjarne-priv@holmedal.net>
2021-02-04 14:06:51 +0100

    Add project files.

---
+++ radclientlib/Client.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+using System.Threading.Tasks;
+
+namespace net.holmedal
+{
+    public class Client
+    {
+        public event EventHandler<UserAvalableArgs> UserAvalable;
+        private Uri _BaseURI;
+        private HttpClient _AClient = new HttpClient();
+        public Client(string BaseUrl)
+        {
+            _BaseURI = new Uri(BaseUrl);
+            _AClient.BaseAddress = _BaseURI ;
+        }
+        public async void GetUserAsync(string user)
+        {
+            _AClient.DefaultRequestHeaders.Accept
+  .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var resp = await _AClient.GetAsync("GetUser?usr=" + user);
+            User a = await resp.Content.ReadAsAsync<User>();
+            _AClient.DefaultRequestHeaders.Accept.Remove(new MediaTypeWithQualityHeaderValue("application/json"));
+            if (a.Password != null)
+            {
+                UserAvalable(this, new UserAvalableArgs(a.Name, a.Password, a.Vlan.Value));
+            }
+            else { }
+        }
+           
+        public async  Task UpdateUser(string UserName, string Password,int vlan)
+        {
+            User a = new User() {Name =UserName,Password = Password, Vlan=vlan };
+            
+            await _AClient.PutAsJsonAsync("UpdateUser",a);
+            
+        }
+        public async Task AddUser(string UserName,string Password,int vlan)
+        {
+            User a = new User()
+            {
+                Name = UserName,
+                Password = Password,
+                Vlan = vlan
+            };
+            await _AClient.PostAsJsonAsync("AddUser",a);
+        }
+
+
+    }
+}
---
+++ radclientlib/UserAvalableArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace net.holmedal
+{
+    public class UserAvalableArgs : EventArgs
+    {
+#nullable enable
+        private string? _Username,_Password;
+        private int? _Vlan;
+        public String Usernmae { get { return _Username; } }
+        public String Password { get { return _Password; } }
+        public int? Vlan { get { return _Vlan; } }
+        public UserAvalableArgs(String? Username,string? Password,int? Vlan)
+        {
+            _Username = Username;
+            _Password = Password;
+            _Vlan = Vlan;
+        }
+    }
+}
